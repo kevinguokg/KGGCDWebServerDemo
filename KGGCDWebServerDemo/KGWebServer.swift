65975e8a30d90ee355b951b7a71ecd92f9dfa774
Kevin Guo <kevinguokg@hotmail.com>
2016-09-14 12:46:36 -0700

    some refactor

--- KGGCDWebServerDemo/KGWebServer.swift
+++ KGGCDWebServerDemo/KGWebServer.swift
@@ -15,37 +15,40 @@ typealias fetchPhotoIdCompletionHandler = (success: Bool, url: NSURL?) -> Void
 class KGWebServer {
     
     var webServer : GCDWebServer!
+    let port : UInt = 8089
     
     private static let _sharedWebServer = KGWebServer()
+    
+    static func getWebServer() -> KGWebServer{
+        return KGWebServer._sharedWebServer
+    }
     
     private init() {
         self.webServer = GCDWebServer()
         
+        // /photos/
         self.webServer.addHandlerForMethod("GET", pathRegex: "^/photos(/)*$", requestClass: GCDWebServerRequest.self) { (request, completionBlock) in
-            //completionBlock(GCDWebServerDataResponse(HTML:"<html><body><h1>Testing...</h1></body></html>"))
             print("CASE /photos")
             completionBlock(GCDWebServerDataResponse(HTML:"<html><body><h1>CASE /photos</h1></body></html>"))
 
         }
         
+        // /photos/{id}/
         self.webServer.addHandlerForMethod("GET", pathRegex: "^/photos/((\\w|\\d)-?)+(/)*$", requestClass: GCDWebServerRequest.self) { (request, completionBlock) in
             print("CASE /photos/id")
-            //completionBlock(GCDWebServerDataResponse(HTML:"<html><body><h1>CASE /photos/id</h1></body></html>"))
             
             // TODO: NSRegular Expressions
-            let photoPath = request.path.stringByReplacingOccurrencesOfString("/photos/", withString: "")
-            var photoPath2 : String? = nil
+            var photoPath : String? = nil
             do {
                 //let regex = try NSRegularExpression(pattern: "/(\\w+)\\/(.+/*)\\/(.*)", options: .CaseInsensitive)
                 let regex = try NSRegularExpression(pattern: "/(photos)\\/(((\\w|\\d)-?)+)", options: .CaseInsensitive)
                 let result = regex.matchesInString(request.path, options: .Anchored, range: NSMakeRange(0, request.path.characters.count))
                 
-                let size = result.count
                 for match in result {
                     let matchRange = match.rangeAtIndex(2)
                     let matchString = (request.path as NSString).substringWithRange(matchRange)
                     print(matchString)
-                    photoPath2 = matchString
+                    photoPath = matchString
                 }
                 
                 
@@ -53,8 +56,7 @@ class KGWebServer {
                 print("Error in processing url using regular expressions")
             }
            
-            
-            if let photoPath = photoPath2 {
+            if let photoPath = photoPath {
                 let options = PHFetchOptions()
                 options.sortDescriptors = [NSSortDescriptor.init(key: "creationDate", ascending: false)]
                 let set = PHAsset.fetchAssetsWithLocalIdentifiers([photoPath], options: options)
@@ -70,22 +72,24 @@ class KGWebServer {
                     completionBlock(GCDWebServerDataResponse(HTML:"<html><body><h1>Sorry, your requested photo isn't found</h1></body></html>"))
                 }
             }
+            //completionBlock(GCDWebServerDataResponse(HTML:"<html><body><h1>CASE /photos/id</h1></body></html>"))
         }
         
+        
+        // /photos/{id}/thumbnails
         self.webServer.addHandlerForMethod("GET", pathRegex: "^/photos/((\\w|\\d)-?)+/thumbnails$", requestClass: GCDWebServerRequest.self) { (request, completionBlock) in
             print("CASE /photos/id/thumbnails")
             
-            var photoPath2 : String? = nil
+            var photoPath : String? = nil
             do {
                 let regex = try NSRegularExpression(pattern: "/(photos)\\/(((\\w|\\d)-?)+)\\/", options: .CaseInsensitive)
                 let result = regex.matchesInString(request.path, options: .Anchored, range: NSMakeRange(0, request.path.characters.count))
                 
-                let size = result.count
                 for match in result {
                     let matchRange = match.rangeAtIndex(2)
                     let matchString = (request.path as NSString).substringWithRange(matchRange)
                     print(matchString)
-                    photoPath2 = matchString
+                    photoPath = matchString
                 }
                 
                 
@@ -93,7 +97,7 @@ class KGWebServer {
                 print("Error in processing url using regular expressions")
             }
 
-            if let photoPath = photoPath2 {
+            if let photoPath = photoPath {
                 let options = PHFetchOptions()
                 options.sortDescriptors = [NSSortDescriptor.init(key: "creationDate", ascending: false)]
                 let set = PHAsset.fetchAssetsWithLocalIdentifiers([photoPath], options: options)
@@ -112,17 +116,12 @@ class KGWebServer {
                     completionBlock(GCDWebServerDataResponse(HTML:"<html><body><h1>Sorry, your requested thumbnail isn't found</h1></body></html>"))
                 }
             }
-            
             //completionBlock(GCDWebServerDataResponse(HTML:"<html><body><h1>CASE /photos/id/thumbnails</h1></body></html>"))
         }
 
-        self.webServer.startWithPort(8089, bonjourName: "GCD Web Server")
+        self.webServer.startWithPort(self.port, bonjourName: "GCD Web Server")
         print("Visit \(self.webServer.serverURL) in your web browser")
 
-    }
-    
-    static func getWebServer() -> KGWebServer{
-        return KGWebServer._sharedWebServer
     }
     
 }
